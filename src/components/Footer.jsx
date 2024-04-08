a06db1006c31757e2abaa0624658458d843e9d99
Vee1807 <125376159+Vee1807@users.noreply.github.com>
2024-04-08 23:41:10 +0400

    Change navbar and footer size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="bg-gray-800 text-white py-2 w-full">
+        <footer className="bg-gray-800 text-white py-1 w-full sm:text-sm text-xs">
             <div className="mx-auto">
                 <div className="text-center">
                     <p>Powered by <a href="https://www.weatherapi.com/" className="text-blue-300 hover:text-blue-400" title="Free Weather API">WeatherAPI.com</a></p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import ThemeToggle from './ThemeToggle.jsx'
 
 const Navbar = () => {
     return (
-        <nav className="flex flex-row justify-between px-5  py-2 items-center text-blue-950 dark:text-white bg-white dark:bg-gray-900 shadow-md">
+        <nav className="sm:h-14 h-11 flex flex-row justify-between px-5  py-2 items-center text-blue-950 dark:text-white bg-white dark:bg-gray-900 shadow-md">
             <h1 className="sm:text-2xl text-xl font-black">Weather App</h1>
             <ThemeToggle />
         </nav>
