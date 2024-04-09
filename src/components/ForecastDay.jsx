01dd7451160a5e86a05885ee8d0da4af24c11c3a
Vee1807 <125376159+Vee1807@users.noreply.github.com>
2024-04-09 19:31:39 +0400

    Add accordion functionality to ForecastDay component with animated content expansion

--- src/components/ForecastDay.jsx
+++ src/components/ForecastDay.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import WeatherIcons from '../data/WeatherIcons'
-
+import { useState } from 'react'
 const getDate = (dateStr) => {
     const date = new Date(dateStr)
 
@@ -18,6 +18,9 @@ const getDate = (dateStr) => {
 }
 
 const ForecastDay = ({ dayWeather, index }) => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
     const date = getDate(dayWeather.date)
     const weatherCode = dayWeather.day.condition.code
     let icon = ""
@@ -29,8 +32,8 @@ const ForecastDay = ({ dayWeather, index }) => {
     }
 
     return (
-        <div className="py-3 px-7 border-b-2 dark:border-gray-900 w-full text-black dark:text-white">
-            <div className="grid grid-cols-12">
+        <div className="flex flex-col py-3 px-7 border-b-2 h-full dark:border-gray-900 w-full text-black dark:text-white">
+            <div className="grid grid-cols-12 flex-grow">
                 <div className='col-span-3 flex flex-col items-start justify-center sm:text-xl xs:text-base text-sm'>
                     <b className='font-black text-amber-600 dark:text-amber-300'>{index === 0 ? 'TODAY' : date.day}</b>
                     <p className='text-gray-600 dark:text-gray-400 sm:text-base xs:text-sm text-xs'>{date.date}</p>
@@ -40,28 +43,38 @@ const ForecastDay = ({ dayWeather, index }) => {
                     <i className={icon + ' sm:text-5xl xs:text-4xl text-3xl mr-2'}></i>
                     <div className="h-fit flex flex-col ">
                         <b className="sm:text-2xl xs:text-xl text-base">{`${dayWeather.day.maxtemp_c}°`}</b>
-                        <div className="text-gray-600 dark:text-gray-400 sm:text-xl xs:text-base text-sm">{`${dayWeather.day.mintemp_c}°`}</div>
+                        <div className="text-gray-600 dark:text-gray-400 sm:text-xl xs:text-base text-sm">{`${dayWeather.day.mintemp_c}°C`}</div>
                     </div>
                 </div>
 
-                <div className="col-span-4 flex items-center justify-self-end ">
+                <div className="col-span-4 flex items-center justify-self-end">
                     <h3 className="font-bold sm:text-xl xs:text-base text-sm text-right">{dayWeather.day.condition.text}</h3>
                 </div>
 
             </div>
-            <div className="grid grid-cols-2 mt-5 sm:text-base xs-text-sm text-xs text-gray-600 dark:text-gray-400">
-                <div>
-                    <p><b className="text-gray-700 dark:text-gray-300">Wind: </b> {dayWeather.day.maxwind_kph} km/h</p>
-                    <p><b className="text-gray-700 dark:text-gray-300">Humidity: </b> {dayWeather.day.avghumidity}%</p>
-                </div>
-
-                <div className='justify-self-end text-right'>
-                    <p><b className="text-gray-700 dark:text-gray-300">Rain: </b> {dayWeather.day.daily_chance_of_rain}%</p>
-                    <p><b className="text-gray-700 dark:text-gray-300">Snow: </b> {dayWeather.day.daily_chance_of_snow}%</p>
-                </div>
-
-
+            <div className='flex justify-end'>
+            <button style={{WebkitTextStroke: '1px'}} className='font-black text-amber-600 dark:text-amber-300 '
+                onClick={() => setIsOpen(prev => !prev)}>
+                <i className={"bi bi-chevron-" + (isOpen ? 'up' : 'down')}></i>
+            </button>                
             </div>
+            <div className={`overflow-hidden transition-max-height duration-700 ease-in-out ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
+
+            {isOpen &&
+                <div className="grid grid-cols-2 mt-5 sm:text-base xs-text-sm text-xs text-gray-600 dark:text-gray-400">
+                    <div>
+                        <p><b className="text-gray-700 dark:text-gray-300">Wind: </b> {dayWeather.day.maxwind_kph} km/h</p>
+                        <p><b className="text-gray-700 dark:text-gray-300">Humidity: </b> {dayWeather.day.avghumidity}%</p>
+                    </div>
+
+                    <div className='justify-self-end text-right'>
+                        <p><b className="text-gray-700 dark:text-gray-300">Rain: </b> {dayWeather.day.daily_chance_of_rain}%</p>
+                        <p><b className="text-gray-700 dark:text-gray-300">Snow: </b> {dayWeather.day.daily_chance_of_snow}%</p>
+                    </div>
+
+
+                </div>}
+                </div>
         </div>
     )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,7 +10,7 @@ const SearchBar = (props) => {
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                     </svg>
                 </div>
-                <input autoComplete="off" type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border-2 border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none transition duration-200 ease-in-out"
+                <input autoComplete="off" type="search" className="block w-full p-4 ps-10 text-sm text-gray-900 border-2 border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none transition duration-200 ease-in-out"
                     placeholder='Search for city...'
                     onChange={(e) => props.handleCityChange(e.target.value)}
                     value={props.city}>
