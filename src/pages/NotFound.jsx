59e2cc2cee1ce54f04a9864af444e8e9bb6df6b4
vanykardjikian <125376159+vanykardjikian@users.noreply.github.com>
2024-04-22 20:23:12 +0400

    Style NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,9 @@
 const NotFound = () => {
     return (
-        <h1>Page Not Found</h1>
+        <div className="text-blue-950 dark:text-white mx-auto flex-grow flex justify-center items-center mt-20 text-5xl font-bold">
+            <h1>Page Not Found</h1>
+        </div>
+        
     )
 }
 
