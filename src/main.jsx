d48fc9f789971f0b1df77fd4cadfa7ef7e58eb55
vanykardjikian <125376159+vanykardjikian@users.noreply.github.com>
2024-04-22 14:09:50 +0400

    Create routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
+const App = () => {
+	<BrowserRouter>
+	<Routes>
+		<Route path='/' element={<Layout />}>
+			<Route />
+			<Route />
+		</Route>
+	</Routes>
+	</BrowserRouter>
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
